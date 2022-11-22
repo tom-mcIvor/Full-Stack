6df57f0243d5208e1c3f5abecfb372e4a2d3d7e5
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-22 17:19:03 +1300

    add

---
+++ client/components/Advice-giver.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import { AdviceGiver } from '../apis/advice-api'
+
+
+
+useEffect(){
+  AdviceGiver()
+}
+
+export default function Advice() {
+  
+
+return (
+    <div>
+   
+    </div>
+  )
+}
