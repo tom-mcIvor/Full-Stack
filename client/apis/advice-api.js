5783c2dd98bd1d1c7f057eff2098b8f944b7e464
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-26 21:17:25 +1300

    add

--- client/apis/advice-api.js
+++ client/apis/advice-api.js
@@ -1,13 +1,15 @@
 import request from 'superagent'
 
 export function AdviceGiver() {
+  // const hello = JSON.parse('{something: "I am a string"}')
+  // console.log(hello.something)
   return request
     .get('https://api.adviceslip.com/advice')
 
     .then((res) => {
       // console.log('character api', res.body)
       const data = JSON.parse(res.text)
-      console.log(data)
+      console.log(data.slip.advice)
       return res.body
     })
 }
