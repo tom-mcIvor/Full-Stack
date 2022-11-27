e682c7b1957286474f30059313023d1e48ab5334
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-27 20:20:35 +1300

    add

--- client/components/Fruits.jsx
+++ client/components/Fruits.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Fruits(props) {
-  const fruitElements = props.fruits && props.fruits.map(fruit => {
-    return <li key={fruit.id}>
-      {fruit.id}:
-      <input value={fruit.name} onChange={e => props.updateFruitName({
-        ...fruit,
-        name: e.target.value
-      })}></input>
-      <button onClick={() => props.update(fruit)}>save</button>
-      <button onClick={() => props.delete(fruit)}>delete</button>
-    </li>
-  })
-    || <p>Loading...</p>
+  const [fruits, setFruits] = useState(props.fruits)
+
+  useEffect(() => {
+    setFruits(props.fruits)
+  }, [props.fruits])
+
+  const updateFruitName = (fruitId, newName) => {
+    setFruits((prevState) => {
+      return prevState.map((fruit) => {
+        if (fruit.id == fruitId) {
+          return { ...fruit, name: newName }
+        }
+
+        return fruit
+      })
+    })
+  }
+
+  const fruitElements = (fruits &&
+    fruits.map((fruit) => {
+      return (
+        <li key={fruit.id}>
+          {fruit.id}:<input value={fruit.name} onChange={(e) => { updateFruitName(fruit.id, e.target.value) }}></input>
+          <button onClick={() => props.update(fruit)}>save</button>
+          <button onClick={() => props.delete(fruit)}>delete</button>
+        </li>
+      )
+    })) || <p>Loading...</p>
 
   return (
-
     <>
       <div>
         <p>Fruit:</p>
-        <ul>
-          {fruitElements}
-        </ul>
+        <ul>{fruitElements}</ul>
       </div>
     </>
   )
-
 }
-
-
