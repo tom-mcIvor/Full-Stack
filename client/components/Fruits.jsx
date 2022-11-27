1316bc1e1776be5f59efe8718f6773691009e440
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-27 19:16:24 +1300

    add

--- client/components/Fruits.jsx
+++ client/components/Fruits.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getFruits, deleteFruit, updateFruit } from '../services/fruits-api'
 
-
-
 export default function Fruits() {
   // Ready up React state
   const [fruits, setFruits] = useState(null)
@@ -39,7 +37,7 @@ export default function Fruits() {
     ])
   }
 
-  const fruitElements = fruits && fruits.map(fruit => {
+  const fruitElements = this.props.fruits && this.props.fruits.map(fruit => {
     return <li key={fruit.id}>
       {fruit.id}:
       <input value={fruit.name} onChange={e => updateFruits({
